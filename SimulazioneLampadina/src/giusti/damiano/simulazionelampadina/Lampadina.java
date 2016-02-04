831321a4ba1ad39f3a279d2d1f58ee8d43960889
damianogiusti <damianogiusti95@gmail.com>
2016-02-04 10:33:37 +0100

    auto-push script by dg

---
+++ SimulazioneLampadina/src/giusti/damiano/simulazionelampadina/Lampadina.java
@@ -0,0 +1,51 @@
+package giusti.damiano.simulazionelampadina;
+
+class LampadinaBruciataException extends Exception {
+}
+
+public class Lampadina {
+
+	private int cicliMax;
+	private int contaCicli;
+	private boolean stato;
+
+	public Lampadina() {
+		this.cicliMax = 0;
+		this.contaCicli = 0;
+	}
+
+	public Lampadina(int cicliMax) {
+		this.cicliMax = cicliMax;
+		this.contaCicli = 0;
+	}
+
+	public void switchOn() throws LampadinaBruciataException {
+		stato = true;
+		if (++contaCicli == cicliMax)
+			throw new LampadinaBruciataException();
+	}
+
+	public void switchOff() {
+		stato = false;
+	}
+
+	public static void main(String[] args) {
+		Lampadina lampadina = new Lampadina(50000);
+		
+		int cicliProva = 49999;
+
+		for (int i = 0; i < cicliProva; i++) {
+			try {
+				lampadina.switchOn();
+			} catch (LampadinaBruciataException e) {
+				e.printStackTrace();
+			}
+			finally {
+				lampadina.switchOff();
+			}
+		}
+		
+		System.out.println("Eseguiti i cicli di prova");
+	}
+
+}
