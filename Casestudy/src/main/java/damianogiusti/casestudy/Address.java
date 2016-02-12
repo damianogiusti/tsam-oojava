f87f6c0062a5de4db36d8d08a28165deb7c8890f
damianogiusti <damianogiusti95@gmail.com>
2016-02-12 11:36:38 +0100

    auto-push script by dg

---
+++ Casestudy/src/main/java/damianogiusti/casestudy/Address.java
@@ -0,0 +1,65 @@
+package damianogiusti.casestudy;
+
+public class Address {
+	
+	private String tipoVia;
+	private String via;
+	private String nomeVia;
+	
+	public Address() {
+		tipoVia = "";
+		via = "";
+		nomeVia = "";
+	}
+	
+	public Address(String tipoVia, String via, String nomeVia) {
+		this.tipoVia = tipoVia;
+		this.via = via;
+		this.nomeVia = nomeVia;
+	}
+	
+	public String getTipoVia() {
+		return tipoVia;
+	}
+
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getNomeVia() {
+		return nomeVia;
+	}
+
+	public void setNomeVia(String nomeVia) {
+		this.nomeVia = nomeVia;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Address))
+			return false;
+
+		return this.toString().equals(obj.toString());
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return tipoVia + " " + via + " " + nomeVia;
+	}
+	
+	
+
+}
---
+++ Casestudy/src/main/java/test/damianogiusti/casestudy/MainTest.java
@@ -0,0 +1,16 @@
+package test.damianogiusti.casestudy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import damianogiusti.casestudy.Main;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
